451123106569e0992ddccae78160d5a6c4c9c140
Daniel De La Pava <daniel.samkit@gmail.com>
2019-02-21 02:41:43 -0500

    Se refactoriza imports y metodos no usados

--- src/main/java/com/condor/labs/marketplace/service/CartService.java
+++ src/main/java/com/condor/labs/marketplace/service/CartService.java
@@ -16,7 +16,6 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class CartService {
